bbfbab6f2cbf1195b39e81dad92c751ef356a9ad
Michał Szczepaniak <m.szczepaniak.000@gmail.com>
2022-02-05 02:24:48 +0100

    Fix default keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -16,6 +16,12 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(RGB_TOG, RGB_MODE_BREATHE, RGB_MODE_RAINBOW, RGB_MODE_XMAS, RGB_MODE_SWIRL, RESET, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6 )
+    [0] = {
+        { KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5 },
+        { KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T},
+        { KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G},
+        { KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B},
+        { KC_LCTL, KC_LGUI, RGB_TOG, KC_LALT, RESET, KC_SPC}
+    },
 };
 
